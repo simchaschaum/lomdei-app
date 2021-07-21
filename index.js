aff815d86c4a1fff430d41a7282117f59a1b474c
simchaschaum <72506778+simchaschaum@users.noreply.github.com>
2021-07-21 15:14:42 +0300

    Added counter to index.js

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
+// Index Page - the counters
+const counters = document.querySelectorAll('.counter');
+const speed = 50;
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = parseInt(counter.getAttribute('data-target'));
+        const count = parseInt(counter.innerText);
+        const increment = Math.trunc(target/speed);
+        if(count < target){
+            counter.innerText = count + increment;
+            setTimeout(() => {
+                updateCount()
+            }, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+    updateCount();
+})
+
+
+// Videos Page - table of contents
 const contents = document.querySelector('#contents');
 const contentsDropup = document.querySelector('#contentsDropup')
 
